9d215f9e8dfb1981260867f62ce98e91f94fbed8
selemondev <selemondev19@gmail.com>
2023-10-07 18:43:06 +0300

    fix(app): NuxtLink component  attribute
    This pull request is intended to fix NuxtLink's  attribute.

--- components/Icons/Social.vue
+++ components/Icons/Social.vue
@@ -13,7 +13,7 @@ defineProps<{
 <template>
   <div class="flex space-x-3">
     <div v-for="{ id, icon, url } in socialIcons" :key="id">
-      <NuxtLink :path="url" target="_blank" class="flex items-center cursor-pointer">
+      <NuxtLink :to="url" target="_blank" class="flex items-center cursor-pointer">
         <div class="w-10 h-10 bg-[#181818] border-2 border-[#1E1E1E] hover:bg-[#1E1E1E] transition-all duration-200 ease-in grid place-items-center rounded-full">
           <Icon :name="icon" class="text-xl text-white" />
         </div>

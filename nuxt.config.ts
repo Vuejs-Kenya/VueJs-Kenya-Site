e3152023464c2a1a6af9500e9956ae12b11f6c00
selemondev <selemondev19@gmail.com>
2023-10-07 13:33:20 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@vueuse/nuxt'],
+  googleFonts: {
+    families: {
+      Inter: true,
+    },
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: true,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: 'last',
+    viewer: true,
+  },
+  devtools: { enabled: true },
+})

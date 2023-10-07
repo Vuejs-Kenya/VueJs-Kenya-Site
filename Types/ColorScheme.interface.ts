03730e1babaabb3c65888c197b821f7b2400643c
selemondev <selemondev19@gmail.com>
2023-10-07 15:59:19 +0300

    feat(app): add Ticket section
    This pull request is intended to add the Ticket section.

---
+++ Types/ColorScheme.interface.ts
@@ -0,0 +1,10 @@
+export interface Color {
+  id: number
+  name: string
+  backgroundColor: string
+  icon: string
+  borderColor: string
+  textColor: string
+  svgColor: string
+  svgBorder: string
+}

60440e3ebbeb15ab613419b7f041784d499b3b4f
Selemondev <selemondev@selemondev.local>
2024-04-20 15:21:06 +0300

    feat: add Ticket page

--- components/button/CustomComponent.vue
+++ components/button/CustomComponent.vue
@@ -1,13 +1,15 @@
 <script setup lang='ts'>
 defineProps<{
-  label: string
+  label?: string
   loading: boolean
+  iconName?: string
 }>()
 </script>
 
 <template>
   <div
-    class="box-border relative z-30 inline-flex items-center justify-center w-auto px-6 py-2 overflow-hidden font-bold text-white transition-all duration-300 bg-green-600 rounded-md cursor-pointer group hover:ring-1 hover:ring-green-500 ease focus:outline-none"
+    as="button" :class="{ 'cursor-not-allowed hover:ring-0 bg-[#27803C]': loading }"
+    class="box-border relative z-30 inline-flex items-center justify-center w-auto px-6 py-2 overflow-hidden font-normal text-white transition-all duration-300 bg-green-600 rounded-md cursor-pointer group hover:ring-1 hover:ring-green-500 ease focus:outline-none"
   >
     <span
       class="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"
@@ -18,8 +20,11 @@ defineProps<{
     <span v-if="loading" class="relative z-20 flex items-center text-sm">
       <Icon name="ph:spinner" class="text-xl text-white animate-spin" />
     </span>
-    <span v-else class="relative z-20 flex items-center text-sm">
-      {{ label }}
-    </span>
+    <div v-else class="relative z-20 text-sm flex items-center space-x-3">
+      <Icon v-if="iconName" :name="iconName" class="text-xl" />
+      <p class="text-[16px] leading-[22px]">
+        {{ label }}
+      </p>
+    </div>
   </div>
 </template>
---
+++ components/icons/ColorScheme.vue
@@ -0,0 +1,30 @@
+<script setup lang='ts'>
+import type { Color } from '~/types/colorscheme.interface.ts'
+
+const props = defineProps<{
+  colorSchemes: Color[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'handleColorSchemeChange', value: string): void
+}>()
+
+function handleColorScheme(val: string) {
+  emit('handleColorSchemeChange', val)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-6 gap-6">
+    <div v-for="{ id, icon, name } in props.colorSchemes" :key="id" class="grid rounded-lg place-items-center">
+      <div class="group">
+        <button
+          class="w-12 h-12 border dark:border-gray-800 rounded-md grid place-items-center"
+          @click="handleColorScheme(name)"
+        >
+          <Icon :name="icon" class="text-2xl transition-all duration-200 ease-in group-hover:scale-150" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>

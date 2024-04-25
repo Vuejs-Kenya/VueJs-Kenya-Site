2d939314a7bb66726058baf3aa49cdcebe75294a
Selemon Dev <selemon@nathandigital.com>
2024-04-25 11:31:16 +0300

    feat: add pill component

--- components/button/CustomComponent.vue
+++ components/button/CustomComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang='ts'>
 defineProps<{
   label?: string
-  loading: boolean
+  loading?: boolean
   iconName?: string
 }>()
 </script>
---
+++ components/button/PillComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+import { Button } from '@/components/ui/button'
+
+const props = defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div>
+    <Button
+      class="py-1 h-6 rounded-md px-2 border border-green-500 bg-[#EFFDF5] disabled:opacity-40 dark:bg-green-500/20 dark:text-[#54DC82] text-[#48C16A] hover:enabled:bg-[#3BA34A] hover:enabled:text-white duration-200 ease-in text-[13px]"
+    >
+      {{ props.title }}
+    </Button>
+  </div>
+</template>
